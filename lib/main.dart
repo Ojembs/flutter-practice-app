a1c8724fdef3882859d9073fc66fd1d7da76d772
ojembs <awejoseph01@gmail.com>
2024-02-18 23:03:55 +0100

    Rendering quote data dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -2,154 +2,47 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp( const MaterialApp(
-    home: NinjaApp(),
+    home: QuoteApp(),
   ));
 }
 
-class NinjaApp extends StatefulWidget {
-  const NinjaApp({super.key});
+class QuoteApp extends StatefulWidget {
+  const QuoteApp({super.key});
 
   @override
-  State<NinjaApp> createState() => _NinjaAppState();
+  State<QuoteApp> createState() => _QuoteAppState();
 }
 
-class _NinjaAppState extends State<NinjaApp> {
-  int ninjaLevelCount = 0;
+class _QuoteAppState extends State<QuoteApp> {
+  List<String> quotes = [
+    "In the end, it's not the years in your life that count. It's the life in your years. - Abraham Lincoln",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
+    "Life is what happens when you're busy making other plans. - John Lennon"
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text(
-            'Ninja Id Card',
-            style: TextStyle(
-              color: Colors.white
-            ),
+          'Quote App',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.0,
+            fontFamily: 'Inter'
+          ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
 
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {
-          setState(() {
-            ninjaLevelCount += 2;
-          })
-        },
-        backgroundColor: Colors.grey[800],
-        child: Icon(
-          Icons.add,
-          color: Colors.grey[200],
-        ),
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: NetworkImage('https://media.istockphoto.com/id/1284172836/vector/human-skull-poster-illustration.jpg?s=612x612&w=0&k=20&c=2qMf9vnLVZwutJmGrFCOTrc0oH45YCmJhgnH4MgQmMM='),
-                radius: 60.0,
-              ),
-            ),
-
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800],
-            ),
-
-            Text(
-              'Name:',
-              style: TextStyle(
-                color: Colors.grey[500],
-                letterSpacing: 2.0,
-              ),
-            ),
-
-            const SizedBox(height: 10.0),
-
-            Text(
-              'Awe AbdurRahmaan Abu-Khalid',
-              style: TextStyle(
-                color: Colors.amberAccent[200],
-                letterSpacing: 0.0,
-                fontSize: 22.0,
-                fontWeight: FontWeight.bold
-              )
-            ),
-
-            const SizedBox(height: 30.0),
-
-            Text(
-              'Current Ninja Level:',
-              style: TextStyle(
-                color: Colors.grey[500],
-                letterSpacing: 2.0,
-              ),
-            ),
-
-            const SizedBox(height: 10.0),
-
-            Text(
-              '$ninjaLevelCount',
-              style: TextStyle(
-                  color: Colors.amberAccent[200],
-                  letterSpacing: 0.0,
-                  fontSize: 22.0,
-                  fontWeight: FontWeight.bold
-              )
-            ),
-
-            const SizedBox(height: 30.0),
-
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-
-                const SizedBox(width: 10.0),
-
-                Text(
-                  'awejoseph01@gmail.co.uk',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            ),
-
-            const SizedBox(height: 20.0),
-
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.phone,
-                  color: Colors.grey[400],
-                ),
-
-                const SizedBox(width: 10.0),
-
-                Text(
-                  '+66 234 33 221',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList()
       )
     );
   }
 }
-
